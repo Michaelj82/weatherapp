32a2ba61cdb08f04ed0b5369f0e8dca76135b5e4
Michael Johnson <mjj.yoyogi@gmail.com>
2022-10-19 11:35:36 -0400

    basic styling and code done

--- script.js
+++ script.js
@@ -1,12 +1,64 @@
+const main = document.getElementById('weatherinfo')
+
+
 const input = document.getElementById('city');
 const button = document.getElementById('cityButton');
 
 button.addEventListener('click', function(){
 
     let cityInput = input.value;
-    getWeather(cityInput)
+    getWeather(cityInput);
 
 })
+
+
+function kelvinToFarenheit(kelvin){
+    let farenheit = ((kelvin - 273.15) * 1.8) + 32;
+    return farenheit
+}
+
+
+function weatherDOM(weather){
+
+    let coords = `Coordinates: ${weather['coord']['lat']} ${weather['coord']['lon']}`;
+
+    let temperatureKelvin = weather['main']['temp'];
+    let temperature = `Temperature: ${Math.round(kelvinToFarenheit(temperatureKelvin))}`;
+
+    let temperatureFeelsLike = `Feels Like: ${Math.round(kelvinToFarenheit(weather['main']['feels_like']))}`;
+
+    let name = `${weather['name']}, ${weather['sys']['country']}`;
+
+    let weatherDict = weather['weather']['0'] ;
+    let weatherSummary = `${weatherDict['main']}, ${weatherDict['description']}`;
+
+    let coordsHTML = document.createElement('div');
+    coordsHTML.setAttribute('class', 'weatherElement');
+    coordsHTML.textContent = coords
+    
+    let temperatureHTML = document.createElement('div');
+    temperatureHTML.setAttribute('class', 'weatherElement');
+    temperatureHTML.textContent = temperature;
+
+    let temperatureFeelsLikeHTML = document.createElement('div');
+    temperatureFeelsLikeHTML.setAttribute('class', 'weatherElement');
+    temperatureFeelsLikeHTML.textContent = temperatureFeelsLike;
+
+    let nameHTML = document.createElement('div');
+    nameHTML.setAttribute('class', 'weatherElement');
+    nameHTML.textContent = name;
+
+    let weatherSummaryHTML = document.createElement('div');
+    weatherSummaryHTML.setAttribute('class', 'weatherElement');
+    weatherSummaryHTML.textContent = weatherSummary;
+
+    main.append(nameHTML, weatherSummaryHTML, temperatureHTML, temperatureFeelsLikeHTML, coordsHTML)
+
+
+
+
+
+}
 
 
 
@@ -15,9 +67,10 @@ async function getWeather(city){
     try{
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=0fd5cc99b6b27edb9d70e965db3ff28d`);
         const weatherData = await response.json();
-        console.log(weatherData);
+        weatherDOM(weatherData);
     }catch(error){
-        console.log(error)
+        console.log('error');
+        console.log(error);
     }
     
 }
